d71903e41890124cba5d4e1df605d8c7ad303b9c
bryn <brynrd@github.com>
2024-10-01 03:47:53 -0500

    formulario-ang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RegistroFormComponent } from './registro-form/registro-form.component';  // Importa el componente standalone
+import { RegistroFormComponent } from './registro-form/registro-form.component';  
 @Component({
   selector: 'app-root',
   standalone: true,
